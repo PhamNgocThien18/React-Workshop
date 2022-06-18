a6aaea2fb305eedf78f86a67ebb9c3e0c2cc4343
Phạm Ngọc Thiện <39227684+PhamNgocThien18@users.noreply.github.com>
2022-06-18 19:32:24 +0700

    ex4

--- src/App.js
+++ src/App.js
@@ -1,50 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import React from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Welcome from './components/Welcome';
-import Comment from './components/Comment';
-import Avatar from './components/Avatar';
-import Car from './components/Car';
-import Menu from './components/MenuComponent.js';
-import {DISHES} from './shared/dishes';
-
-const comment = {
-  date : new Date(),
-  text:'T hope you enjoy my restaurant ',
-  author:{
-    name : 'alberto',
-    avatarUrl: './images/alberto.png'
-  }
-}
-class App extends React.Component {
-  constructor(props){
+// import { Navbar, NavbarBrand } from 'reactstrap';
+// import Menu from './components/MenuComponent';
+// import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.state ={
-      dishes: DISHES
-    };
+    // this.state = {
+    //   dishes: DISHES
+    // };
   }
-  render(){
+
+  render() {
     return (
       <div className="App">
-        <Navbar dark color='primary'>
-          <div className='container'>
+        {/* <Navbar dark color="primary">
+          <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-
-        <Welcome name="Thien"/>
-        <Welcome name="Thuy Van "/>
-        <Welcome name="Thuy Kieu"/>
-
-        <Comment date={comment.date} text={comment.text} author={comment.author} />
-      
-        <Car />
-        <Menu dishes={this.state.dishes} />
+        <div className="container">
+          <Menu dishes={this.state.dishes} />
+        </div> */}
+        <Main />
       </div>
     );
   }
-  
 }
 
 export default App;
